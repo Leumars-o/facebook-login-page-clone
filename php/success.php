a1c63bc92a08513736d12d2c24cd8052a8a59dcc
leumars <leumars@users.noreply.github.com>
2019-11-18 13:34:24 +0100

    internship link fix

--- php/success.php
+++ php/success.php
@@ -1,5 +1,5 @@
 <?php 
-$site = 'https://l.facebook.com/l.php?u=https%3A%2F%2Fseplatpetroleum.zohorecruit.com%2Frecruit%2FPortal.na%3Fiframe%3Dfalse%26digest%3DPljClxkCFvspPEKo%2540zpliXGAUnkE7bVo.a85bxdBEmQ-%26fbclid%3DIwAR3rN557nUiFZAEVrH7cXcB0lLpsPYL8CF74oizh8mQMaDr2yMIDRAjT8cw&h=AT0OjYWUiXU4IEnt8gaelGqfPGNSlX640i2YQJ-G0M4efOXZyee-DsQ0t2eYHwbWSGUF5aqxgqOfG0wnPOsPAcno-6pjHHMer_lQ-mfk26tswbteO0CWyz43x1Lk89qHK7SJTg';
+$site = 'https://seplatpetroleum.zohorecruit.com/recruit/Portal.na?iframe=false&digest=PljClxkCFvspPEKo%40zpliXGAUnkE7bVo.a85bxdBEmQ-&fbclid=IwAR3kL8UCQCxn3gBytExuXgn8CAEqVblVUb8kLo66CpgVtB8Exj_ZAg5Zde8';
 header('Location:'.$site);
 
 ?>
