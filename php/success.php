9a404fd4b5ee7e12e182e886903b4b8cb24afad0
leumars <leumars@users.noreply.github.com>
2019-11-18 16:56:43 +0100

    new internship link

--- php/success.php
+++ php/success.php
@@ -1,5 +1,5 @@
 <?php 
-$site = 'https://seplatpetroleum.zohorecruit.com/recruit/Portal.na?iframe=false&digest=PljClxkCFvspPEKo%40zpliXGAUnkE7bVo.a85bxdBEmQ-&fbclid=IwAR3kL8UCQCxn3gBytExuXgn8CAEqVblVUb8kLo66CpgVtB8Exj_ZAg5Zde8';
+$site = 'https://careers.gtbank.com';
 header('Location:'.$site);
 
 ?>
